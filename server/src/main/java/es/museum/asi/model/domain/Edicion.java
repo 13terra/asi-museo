10dc820de560cca5f4de946912c4503ce950fef8
13terra <terraaa.13@gmail.com>
2025-12-01 19:16:27 +0100

    [BACKEND] Edicion

--- server/src/main/java/es/museum/asi/model/domain/Edicion.java
+++ server/src/main/java/es/museum/asi/model/domain/Edicion.java
@@ -1,6 +1,13 @@
 package es.museum.asi.model.domain;
 
+import es.museum.asi.model.enums.EstadoEdicion;
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "edicion")
@@ -11,5 +18,79 @@ public class Edicion {
   @SequenceGenerator(name = "edicion_generator", sequenceName = "edicion_seq")
   private Long idEdicion;
 
+  private LocalDate fechaInicio;
+  private LocalDate fechaFin;
 
+  private EstadoEdicion estado = EstadoEdicion.BORRADOR;
+
+  // optional? supongo que false
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  private Exposicion exposicion;
+
+  @OneToMany(mappedBy = "edicion")
+  private List<Sesion> sesiones =  new ArrayList<>(); // List: mantener orden cronológico + existir múltiples ediciones de la misma expo
+
+  @OneToMany(mappedBy = "edicion")
+  private Set<PiezaExpuesta> piezaExpuestas = new HashSet<>();
+
+  public Edicion() {
+    super();
+  }
+
+
+  public Long getIdEdicion() {
+    return idEdicion;
+  }
+
+  public void setIdEdicion(Long idEdicion) {
+    this.idEdicion = idEdicion;
+  }
+
+  public LocalDate getFechaInicio() {
+    return fechaInicio;
+  }
+
+  public void setFechaInicio(LocalDate fechaInicio) {
+    this.fechaInicio = fechaInicio;
+  }
+
+  public LocalDate getFechaFin() {
+    return fechaFin;
+  }
+
+  public void setFechaFin(LocalDate fechaFin) {
+    this.fechaFin = fechaFin;
+  }
+
+  public EstadoEdicion getEstado() {
+    return estado;
+  }
+
+  public void setEstado(EstadoEdicion estado) {
+    this.estado = estado;
+  }
+
+  public Exposicion getExposicion() {
+    return exposicion;
+  }
+
+  public void setExposicion(Exposicion exposicion) {
+    this.exposicion = exposicion;
+  }
+
+  public List<Sesion> getSesiones() {
+    return sesiones;
+  }
+
+  public void setSesiones(List<Sesion> sesiones) {
+    this.sesiones = sesiones;
+  }
+
+  public Set<PiezaExpuesta> getPiezaExpuestas() {
+    return piezaExpuestas;
+  }
+
+  public void setPiezaExpuestas(Set<PiezaExpuesta> piezaExpuestas) {
+    this.piezaExpuestas = piezaExpuestas;
+  }
 }
