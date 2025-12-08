83a0b1a5010f92446fdb9f6621f682a6ec9f002e
13terra <terraaa.13@gmail.com>
2025-12-08 21:02:15 +0100

    [Services] UserService v1

--- server/src/main/java/es/museum/asi/model/service/UserService.java
+++ server/src/main/java/es/museum/asi/model/service/UserService.java
@@ -1,8 +1,19 @@
 package es.museum.asi.model.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import es.museum.asi.model.domain.User;
+import es.museum.asi.model.enums.EstadoUser;
+import es.museum.asi.model.enums.UserAuthority;
+import es.museum.asi.model.exception.NotFoundException;
+import es.museum.asi.model.exception.OperationNotAllowed;
+import es.museum.asi.model.exception.UserLoginExistsException;
+import es.museum.asi.model.service.dto.UserDTOCompleto;
+import es.museum.asi.model.service.dto.UserDTOPrivate;
+import es.museum.asi.model.service.dto.UserDTOPublic;
+import es.museum.asi.repository.GestionDao;
+import es.museum.asi.repository.ReservaDao;
+import es.museum.asi.repository.UserDao;
+import es.museum.asi.security.SecurityUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,111 +21,244 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import es.museum.asi.model.domain.User;
-import es.museum.asi.model.enums.UserAuthority;
-import es.museum.asi.model.exception.NotFoundException;
-import es.museum.asi.model.exception.OperationNotAllowed;
-import es.museum.asi.model.exception.UserLoginExistsException;
-import es.museum.asi.model.repository.UserDao;
-import es.museum.asi.dto.UserDTOPrivate;
-import es.museum.asi.dto.UserDTOPublic;
-import es.museum.asi.security.SecurityUtils;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true, rollbackFor = Exception.class)
 public class UserService {
 
   @Autowired
-  private UserDao userDAO;
+  private UserDao userDao;
 
   @Autowired
-  private NoteDao noteDAO;
+  private ReservaDao reservaDao;
+
+  @Autowired
+  private GestionDao gestionDao;
 
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  /**
+   * HU 4 - Listado de usuarios (sólo para ADMIN)
+   * Filtra por rol o estado según parámetros
+   */
   @PreAuthorize("hasAuthority('ADMIN')")
   public List<UserDTOPublic> findAll() {
-    Stream<UserDTOPublic> users = userDAO.findAll().stream().map(user -> new UserDTOPublic(user));
-    if (SecurityUtils.getCurrentUserIsAdmin()) {
-      return users.collect(Collectors.toList());
+    return userDao.findAll().stream().map(UserDTOPublic::new).collect(Collectors.toList());
+  }
+
+  /**
+   * HU 4 - Listado de usuarios por autoridad
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  public List<UserDTOPublic> findByAutoridad(UserAuthority autoridad) {
+    return userDao.findAll().stream().filter(user -> user.getAutoridad().equals(autoridad)).map(UserDTOPublic::new).collect(Collectors.toList());
+  }
+
+  /**
+   * HU 4 - Listado de usuarios por estado
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  public List<UserDTOPublic> findByEstado(EstadoUser estado) {
+    return userDao.findAll().stream().filter(user -> user.getEstado().equals(estado)).map(UserDTOPublic::new).collect(Collectors.toList());
+  }
+
+  /**
+   * Obtener usuario por ID con toda su información
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  public UserDTOCompleto findOne(Long idUser) throws NotFoundException {
+    User user = userDao.findById(idUser);
+    if(user == null) {
+      throw new NotFoundException(idUser.toString(), User.class);
     }
-    return users.filter(user -> user.isActive()).collect(Collectors.toList());
+    return new UserDTOCompleto(user);
+  }
+
+  /**
+   * HU1 - Registro de visitante (VISITANTE por defecto)
+   * Accesible sin autenticación
+   */
+  @Transactional(readOnly = false)
+  public UserDTOPrivate registerUser(String login, String contrasena) throws UserLoginExistsException {
+    return registerUser(login, contrasena, UserAuthority.VISITANTE);
+  }
+
+  /**
+   * HU5 - Crear usuarios con autoridad específica, sólo ADMIN puede hacerlo
+   * (ADMIN, GESTOR o VISITANTE)
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @Transactional(readOnly = false)
+  public UserDTOPrivate createUser(String login, String contrasena, UserAuthority autoridad) throws UserLoginExistsException {
+    return registerUser(login, contrasena, autoridad);
   }
 
   @PreAuthorize("hasAuthority('ADMIN')")
-  public UserWithNotesDTO findOne(Long id) throws NotFoundException {
-    User user = userDAO.findById(id);
-    if (user == null) {
-      throw new NotFoundException(id.toString(), User.class);
-    }
-    return new UserWithNotesDTO(user);
-  }
-
   @Transactional(readOnly = false)
-  public void registerUser(String login, String password) throws UserLoginExistsException {
-    registerUser(login, password, false);
-  }
-
-  @Transactional(readOnly = false)
-  public void registerUser(String login, String password, boolean isAdmin) throws UserLoginExistsException {
-    if (userDAO.findByLogin(login) != null) {
+  public UserDTOPrivate registerUser(String login, String contrasena, UserAuthority autoridad)
+    throws UserLoginExistsException  {
+    if(userDao.findByLogin(login) != null) {
       throw new UserLoginExistsException(login);
     }
 
     User user = new User();
-    String encryptedPassword = passwordEncoder.encode(password);
+    String encryptedPassword = passwordEncoder.encode(contrasena);
 
     user.setLogin(login);
-    user.setPassword(encryptedPassword);
-    user.setAuthority(UserAuthority.USER);
-    if (isAdmin) {
-      user.setAuthority(UserAuthority.ADMIN);
-    }
+    user.setContrasena(encryptedPassword);
+    user.setAutoridad(autoridad);
+    user.setEstado(EstadoUser.ACTIVO);
 
-    userDAO.create(user);
+    userDao.create(user);
+    return new UserDTOPrivate(user);
   }
 
+  /**
+   * HU6 - Editar usuario (solo ADMIN)
+   * Permite modificar login, contrasena, autoridad y estado
+   * */
   @PreAuthorize("hasAuthority('ADMIN')")
   @Transactional(readOnly = false)
-  public UserDTOPublic updateActive(Long id, boolean active) throws NotFoundException, OperationNotAllowed {
-    User user = userDAO.findById(id);
-    if (user == null) {
-      throw new NotFoundException(id.toString(), User.class);
+  public UserDTOPublic updateUser(Long idUser,String login, String contrasena, UserAuthority autoridad, EstadoUser estado)
+    throws NotFoundException, OperationNotAllowed {
+    User user = userDao.findById(idUser);
+    if(user == null) {
+      throw new NotFoundException(idUser.toString(), User.class);
     }
 
-    UserDTOPrivate currentUser = getCurrentUserWithAuthority();
-    if (currentUser.getId().equals(user.getId())) {
-      throw new OperationNotAllowed("The user cannot activate/deactive itself");
+    // Validar restricción: no cambiar GESTOR/ADMIN a VISITANTE si tiene exposiciones
+    if ((user.getAutoridad() == UserAuthority.GESTOR || user.getAutoridad() == UserAuthority.ADMIN)
+    && autoridad == UserAuthority.VISITANTE) {
+      if(!gestionDao.findByUser(idUser).isEmpty()) {
+        throw new OperationNotAllowed("No se puede cambiar la autoridad de un GESTOR o ADMIN a VISITANTE si tiene exposiciones asignadas.");
+      }
     }
 
-    user.setActive(active);
-    userDAO.update(user);
+    // Actualización de campos
+    if (login != null && !login.equals(user.getLogin())) {
+      // Validamos que el nuevo login no existe
+      if (userDao.findByLogin(login) != null) {
+        throw new OperationNotAllowed("El login " + login + " ya existe en el sistema.");
+      }
+      user.setLogin(login);
+    }
+
+    if(contrasena != null && !contrasena.isEmpty()) {
+      user.setContrasena(passwordEncoder.encode(contrasena));
+    }
+
+    if(autoridad != null) {
+      user.setAutoridad(autoridad);
+    }
+
+    if(estado != null) {
+      user.setEstado(estado);
+    }
+    userDao.update(user);
     return new UserDTOPublic(user);
   }
 
+  /**
+   * HU8 - Desactivar usuario (sólo ADMIN)
+   * Suspensión temporal del acceso
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @Transactional(readOnly = false)
+  public UserDTOPublic deactivateUser(Long idUser)
+    throws NotFoundException, OperationNotAllowed {
+    return updateUserEstado(idUser, EstadoUser.INACTIVO);
+  }
+
+  /**
+   * HU8 - Activar usuario (sólo ADMIN)
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @Transactional(readOnly = false)
+  public UserDTOPublic activateUser(Long idUser)
+    throws NotFoundException, OperationNotAllowed {
+    return updateUserEstado(idUser, EstadoUser.ACTIVO);
+  }
+
+  /**
+   * Método privado para cambiar estado usuario
+   */
+
+  private UserDTOPublic updateUserEstado(Long idUser, EstadoUser estado)
+    throws NotFoundException, OperationNotAllowed {
+    User user = userDao.findById(idUser);
+    if(user == null) {
+      throw new NotFoundException(idUser.toString(), User.class);
+    }
+    UserDTOPrivate currentUser = getCurrentUserWithAuthority();
+    if (currentUser.getId().equals(user.getIdUser())) {
+      throw new OperationNotAllowed("El usuario no puede activar/desactivar su propia cuenta.");
+    }
+
+    user.setEstado(estado);
+    userDao.update(user);
+    return new UserDTOPublic(user);
+  }
+
+  /**
+   * HU7 - Eliminar usuario (sólo ADMIN)
+   * Validar que no sea el admin semilla ni el último admin activ
+   */
+  @PreAuthorize("hasAuthority('ADMIN')")
+  @Transactional(readOnly = false)
+  public void deleteById(Long idUser) throws NotFoundException, OperationNotAllowed {
+    User theUser =  userDao.findById(idUser);
+    if(theUser == null) {
+      throw new NotFoundException(idUser.toString(), User.class);
+    }
+
+    // No eliminar su propia cuenta
+    UserDTOPrivate currentUser = getCurrentUserWithAuthority();
+    if (currentUser.getId().equals(theUser.getIdUser())) {
+      throw new OperationNotAllowed("El usuario no puede eliminarse a sí mismo.");
+    }
+
+    // No eliminar admin semilla
+    if ("admin".equals(theUser.getLogin())) {
+      throw new OperationNotAllowed("No se puede eliminar al admin semilla");
+    }
+
+    // Debe existir al menos 1 admin activo
+    if (theUser.getAutoridad() == UserAuthority.ADMIN) {
+      long activeAdmins = userDao.findAll().stream().filter(u -> u.getAutoridad() == UserAuthority.ADMIN
+      && u.getEstado() == EstadoUser.ACTIVO).count(); //long porque .count devuelve un long
+
+      if (activeAdmins <= 1) {
+        throw new OperationNotAllowed("Debe existir al menos un administrador activo en el sistema");
+      }
+    }
+
+    //Eliminar gestiones asociadas
+    gestionDao.findByUser(idUser).forEach(gestion -> gestionDao.delete(gestion));
+
+    //Eliminar reservas y entradas asociadas (en cascada)
+    reservaDao.findByUser(idUser).forEach(reserva -> reservaDao.delete(reserva));
+
+    userDao.delete(theUser);
+  }
+
+  // Obtener usuario actual autenticado con autoridad
   public UserDTOPrivate getCurrentUserWithAuthority() {
     String currentUserLogin = SecurityUtils.getCurrentUserLogin();
     if (currentUserLogin != null) {
-      return new UserDTOPrivate(userDAO.findByLogin(currentUserLogin));
+      User user = userDao.findByLogin(currentUserLogin);
+      if(user != null) {
+        return new UserDTOPrivate(user);
+      }
     }
     return null;
   }
 
-  @PreAuthorize("hasAuthority('ADMIN')")
-  @Transactional(readOnly = false)
-  public void deleteById(Long id) throws NotFoundException, OperationNotAllowed {
-    User theUser = userDAO.findById(id);
-    if (theUser == null) {
-      throw new NotFoundException(id.toString(), User.class);
-    }
-    UserDTOPrivate currentUser = getCurrentUserWithAuthority();
-    if (currentUser.getId().equals(theUser.getId())) {
-      throw new OperationNotAllowed("The user cannot remove itself");
-    }
 
-    theUser.getNotes().forEach(n -> noteDAO.delete(n));
-    userDAO.delete(theUser);
+  // Buscar usuario por login
+  public User findByLogin(String login) {
+    return userDao.findByLogin(login);
   }
-
 }
