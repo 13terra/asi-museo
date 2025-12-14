695dfd645e94005c0c2d85881eec9e44920740e4
13terra <terraaa.13@gmail.com>
2025-12-14 18:28:43 +0100

    [MS] Se coló un DTO al pushear, además, se realizaron pruebas en Postman antes de subir los cambios

--- met-microservice/src/main/java/es/museo/met/dto/ObraSimplificadaDTO.java
+++ met-microservice/src/main/java/es/museo/met/dto/ObraSimplificadaDTO.java
@@ -1,4 +1,46 @@
 package es.museo.met.dto;
 
+/**
+ * DTO simplificado para listados de búsqueda (HU48)
+ */
 public class ObraSimplificadaDTO {
+    private Long idExterno;
+    private String titulo;
+    private String autor;
+    private String imagen;
+
+    public ObraSimplificadaDTO() {}
+
+
+    public Long getIdExterno() {
+        return idExterno;
+    }
+
+    public void setIdExterno(Long idExterno) {
+        this.idExterno = idExterno;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
